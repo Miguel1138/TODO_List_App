2bc59659bf3c2229d4c2742adb3965bada30db21
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-07-14 09:47:51 -0300

    Initial Commit
    Signed-off-by: Miguel S. da Silva <miguels.dasilva00@gmail.com>

---
+++ app/src/main/java/com/miguelsantos/todewit/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.miguelsantos.todewit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}

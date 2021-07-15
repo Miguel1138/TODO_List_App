df393a8ddd3dc029e83a8cb461de15c63e38b8c6
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-07-15 09:58:56 -0300

    Move the content_add_task.xml to activity_add_task.xml
    - Made this change since view binding doesn't recognize components in the tag <include>
    - Saving this state now to implement viewBinding
    
    Signed-off-by: Miguel S. da Silva <miguels.dasilva00@gmail.com>

---
+++ app/src/main/java/com/miguelsantos/todewit/ui/AddTaskActivity.kt
@@ -0,0 +1,13 @@
+package com.miguelsantos.todewit.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.miguelsantos.todewit.R
+
+class AddTaskActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}

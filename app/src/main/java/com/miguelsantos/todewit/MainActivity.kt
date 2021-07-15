0acb6276de3c4dca76ff95fff8b9115f82bbf65a
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-07-15 10:36:14 -0300

    Insert datePicker and viewBinding to activity_add_task.xml
    - Create AppExtensions.kt for Date treatment
    - The app now inflates a datePicker in the edtDate field, also showing the selected date in the textField.
    - Implement viewBinding in both activities so far.
    
    Signed-off-by: Miguel S. da Silva <miguels.dasilva00@gmail.com>

--- app/src/main/java/com/miguelsantos/todewit/MainActivity.kt
+++ app/src/main/java/com/miguelsantos/todewit/MainActivity.kt
@@ -2,12 +2,16 @@ package com.miguelsantos.todewit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.miguelsantos.todewit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 }
--- app/src/main/java/com/miguelsantos/todewit/ui/AddTaskActivity.kt
+++ app/src/main/java/com/miguelsantos/todewit/ui/AddTaskActivity.kt
@@ -2,12 +2,38 @@ package com.miguelsantos.todewit.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.miguelsantos.todewit.R
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.miguelsantos.todewit.databinding.ActivityAddTaskBinding
+import com.miguelsantos.todewit.extensions.format
+import com.miguelsantos.todewit.extensions.text
+import java.util.*
 
-class AddTaskActivity: AppCompatActivity() {
+class AddTaskActivity : AppCompatActivity() {
+
+    companion object {
+        private const val DATE_PICKER_TAG: String = "date_picker_tag"
+    }
+
+    private lateinit var binding: ActivityAddTaskBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
     }
+
+    private fun setListeners() {
+        binding.taskInputLayoutDate.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time) * -1
+                binding.taskInputLayoutDate.text =  Date(it + offset).format()
+            }
+            datePicker.show(supportFragmentManager, Companion.DATE_PICKER_TAG)
+        }
+    }
+
 }
